b90634bd0279f97c122abb128160070e45b7331e
kittycong <mintheather@gmail.com>
2026-06-10 13:56:07 +0900

    fix: rewrite unknown routes to home

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "/"
+        }
+      ]
+    };
+  }
+};
 
 module.exports = nextConfig;
